f45dba5aa0559c6c975b152484d5342fa8575d42
James Sangalli <j.l.sangalli@gmail.com>
2021-09-21 14:22:31 +1000

    add constructor with doc

--- util/selector.js
+++ util/selector.js
@@ -1,4 +1,20 @@
+const request = require("superagent");
+const endpoints = {
+    "DOT": "https://polkadot.api.subscan.io",
+    "KSM": "https://kusama.api.subscan.io"
+}
+
 module.exports = class Selector {
+
+    /*
+    * @param apiKey - the api key for the subscan endpoint
+    * @param network - the network to use for this tool e.g. DOT
+    * */
+    constructor(apiKey, network) {
+        this.apiKey = apiKey;
+        this.network = network;
+        this.endpoint = endpoints[network];
+    }
 
     /*
     * @dev check if a validator has a verified identity
@@ -33,5 +49,5 @@ module.exports = class Selector {
     * @param id - the identifier for the validator
     * @returns boolean - true if has been slashed else false
     * */
-    async getHasBeenSlashed(id) {}
+    async getHasBeenSlashed(id) {};
 }
