d48a0f503b436d9370df0ce051bea189cb376bd9
James Sangalli <j.l.sangalli@gmail.com>
2021-09-21 13:40:08 +1000

    npm init

---
+++ test/selector.test.js
@@ -0,0 +1,14 @@
+const { chai, expect } = require('chai');
+const Selector = require("../utils/selector");
+
+describe("helpers functionality", () => {
+
+    before(async () => {
+
+    });
+
+    it("should be able to find a validator", async () => {
+
+    });
+
+});
