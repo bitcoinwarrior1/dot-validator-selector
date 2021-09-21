eb4d4b1c63322a556cc1b3b523a4907f57fb7be0
James Sangalli <j.l.sangalli@gmail.com>
2021-09-21 13:54:45 +1000

    boilerplate and document

--- test/selector.test.js
+++ test/selector.test.js
@@ -1,13 +1,29 @@
 const { chai, expect } = require('chai');
 const Selector = require("../utils/selector");
 
-describe("helpers functionality", () => {
+describe("selector functionality", () => {
 
     before(async () => {
 
     });
 
-    it("should be able to find a validator", async () => {
+    it("should be able to find a validator that meets all the criteria", async () => {
+
+    });
+
+    it("should be able to check if the validator has a verified id", async() => {
+
+    });
+
+    it("should be able to get a validators commission", async() => {
+
+    });
+
+    it("should be able to get a validators stake in the pool", async() => {
+
+    });
+
+    it("should be able to check if the validator has been slashed before", async() => {
 
     });
 
--- util/selector.js
+++ util/selector.js
@@ -1,3 +1,37 @@
 module.exports = class Selector {
 
+    /*
+    * @dev check if a validator has a verified identity
+    * @param id - the identifier for the validator
+    * @returns boolean - true if identity is verified else false
+    * */
+    async getIsVerified(id) {};
+
+    /*
+    * @dev return validators that meet the criteria as set in the README
+    * @param amount - the max amount of validators to return e.g. 12 for DOT
+    * @returns an array of objects containing validator info that meets this criteria
+    * */
+    async getValidators(amount) {};
+
+    /*
+    * @dev get the commission rate of the validator
+    * @param id - the identifier for the validator
+    * @returns a number with the commission rate
+    * */
+    async getCommissionRate(id) {};
+
+    /*
+    * @dev see how much skin in the game that a validator has by checking their stake in their own pool
+    * @param id - the identifier for the validator
+    * @returns a number with the amount of tokens they have staked
+    * */
+    async getStakeInPool(id) {};
+
+    /*
+    * @dev check if a validator has been slashed before
+    * @param id - the identifier for the validator
+    * @returns boolean - true if has been slashed else false
+    * */
+    async getHasBeenSlashed(id) {}
 }
