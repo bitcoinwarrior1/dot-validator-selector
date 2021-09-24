2e03130acfc0e57ebfa306240ff8d445c0969f2f
James Sangalli <j.l.sangalli@gmail.com>
2021-09-24 14:28:27 +1000

    add basic sample web page

---
+++ web-sample/index.js
@@ -0,0 +1,12 @@
+const ValidatorSelector = require("../util/ValidatorSelector");
+const { ApiPromise, WsProvider} = require("@polkadot/api");
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const api = await ApiPromise.create({ provider: new WsProvider("wss://polkadot.elara.patract.io") });
+    const selector = new ValidatorSelector(1, 0, api);
+    const validators = await selector.getValidators(12);
+    // TODO make more presentable
+    document.getElementById("validators").innerText = validators.map((v) => {
+        return v.accountId;
+    }).join("\n");
+});
