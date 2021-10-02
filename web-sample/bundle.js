87b3c76aa176c09e70d8f4e36f8f207bfc440942
James Sangalli <j.l.sangalli@gmail.com>
2021-10-02 10:20:11 +1000

    add selection for KSM and DOT on the web sample #12

--- web-sample/bundle.js
+++ web-sample/bundle.js
@@ -76645,13 +76645,26 @@ const ValidatorSelector = require("../util/ValidatorSelector");
 const { ApiPromise, WsProvider} = require("@polkadot/api");
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const api = await ApiPromise.create({ provider: new WsProvider("wss://polkadot.elara.patract.io") });
-    const selector = new ValidatorSelector(api);
-    const validators = await selector.getValidators(12);
-    document.getElementById("validators").innerText = validators.map((v, i) => {
-        return `${++i}. address: ${v.accountId} commission: ${v.commission} name: ${selector.hexToUtf8(v.identity.display.raw.substring(2))}`;
-    }).join("\n");
-    document.getElementById("status").hidden = true;
+
+    document.getElementById("submit").addEventListener("click", async () => {
+        document.getElementById("status").hidden = false;
+        const isKSM = document.getElementById("KSM").checked;
+        let amt = 16;
+        let api;
+        if(isKSM) {
+            api = await ApiPromise.create({ provider: new WsProvider("wss://kusama-rpc.polkadot.io/") });
+            amt = 24;
+        } else {
+            api = await ApiPromise.create({ provider: new WsProvider("wss://polkadot.elara.patract.io") });
+        }
+        const selector = new ValidatorSelector(api);
+        const validators = await selector.getValidators(amt);
+        document.getElementById("validators").innerText = validators.map((v, i) => {
+            return `${++i}. address: ${v.accountId} commission: ${v.commission} name: ${selector.hexToUtf8(v.identity.display.raw.substring(2))}`;
+        }).join("\n");
+        document.getElementById("status").hidden = true;
+    });
+
 });
 },{"../util/ValidatorSelector":990,"@polkadot/api":139}],992:[function(require,module,exports){
 var asn1 = exports;
--- web-sample/index.js
+++ web-sample/index.js
@@ -2,11 +2,24 @@ const ValidatorSelector = require("../util/ValidatorSelector");
 const { ApiPromise, WsProvider} = require("@polkadot/api");
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const api = await ApiPromise.create({ provider: new WsProvider("wss://polkadot.elara.patract.io") });
-    const selector = new ValidatorSelector(api);
-    const validators = await selector.getValidators(12);
-    document.getElementById("validators").innerText = validators.map((v, i) => {
-        return `${++i}. address: ${v.accountId} commission: ${v.commission} name: ${selector.hexToUtf8(v.identity.display.raw.substring(2))}`;
-    }).join("\n");
-    document.getElementById("status").hidden = true;
+
+    document.getElementById("submit").addEventListener("click", async () => {
+        document.getElementById("status").hidden = false;
+        const isKSM = document.getElementById("KSM").checked;
+        let amt = 16;
+        let api;
+        if(isKSM) {
+            api = await ApiPromise.create({ provider: new WsProvider("wss://kusama-rpc.polkadot.io/") });
+            amt = 24;
+        } else {
+            api = await ApiPromise.create({ provider: new WsProvider("wss://polkadot.elara.patract.io") });
+        }
+        const selector = new ValidatorSelector(api);
+        const validators = await selector.getValidators(amt);
+        document.getElementById("validators").innerText = validators.map((v, i) => {
+            return `${++i}. address: ${v.accountId} commission: ${v.commission} name: ${selector.hexToUtf8(v.identity.display.raw.substring(2))}`;
+        }).join("\n");
+        document.getElementById("status").hidden = true;
+    });
+
 });
