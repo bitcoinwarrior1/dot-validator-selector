6290d10d7944e291af915fbef2f09b311ff6c611
James Sangalli <j.l.sangalli@gmail.com>
2022-02-15 19:48:41 +1100

    fix: staked amount is using the wrong decimal #24

--- util/cache.js
+++ util/cache.js
@@ -2,9 +2,9 @@ const { ApiPromise, WsProvider} = require("@polkadot/api");
 const ValidatorSelector = require("./ValidatorSelector");
 const fs = require("fs");
 
-async function cacheValidators(path, nodeUrl, amount) {
+async function cacheValidators(path, nodeUrl, amount, minStaking) {
     const api = await ApiPromise.create({ provider: new WsProvider(nodeUrl) });
-    const selector = new ValidatorSelector(api);
+    const selector = new ValidatorSelector(api, undefined, undefined, minStaking);
     const validators = await selector.getValidators(amount);
     const era = selector.era;
     fs.writeFileSync(`${path}${era}.json`, JSON.stringify(validators, null, 4));
@@ -16,8 +16,8 @@ async function cacheValidators(path, nodeUrl, amount) {
 }
 
 async function main() {
-    await cacheValidators("cache/dot/", "wss://polkadot.api.onfinality.io/ws?apikey=09f0165a-7632-408b-ba81-08f964b607f7", 16);
-    return cacheValidators("cache/ksm/", "wss://kusama.api.onfinality.io/ws?apikey=09f0165a-7632-408b-ba81-08f964b607f7", 24);
+    await cacheValidators("cache/dot/", "wss://polkadot.api.onfinality.io/ws?apikey=09f0165a-7632-408b-ba81-08f964b607f7", 16, 1000);
+    return cacheValidators("cache/ksm/", "wss://kusama.api.onfinality.io/ws?apikey=09f0165a-7632-408b-ba81-08f964b607f7", 24, 100);
 }
 
 main().then(() => { process.exit(0) }).catch(() => { process.exit(-1) });
